b20a835ade252600ee7014a92a3b9098ed8e787a
Oristanio-Lucas <lucas.faes@vindi.com.br>
2024-05-14 12:10:27 -0300

    Implementando todos os cenários de erro 'nota inválida' no teste

--- aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
+++ aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
@@ -2,31 +2,36 @@ require 'spec_base'
 require 'aprovado_ou_reprovado'
 
 describe 'Aprovado ou reprovado' do
-  it 'deve calcular a média de 2 números positivos e retornar aprovado' do
-    expect(media(7, 6)).to eq("aprovado")
+
+  it 'deve calcular a média de 2 números sendo n1 negativo e retornar nota inválida' do
+    expect(media(-0.1,0)).to eq("nota inválida")
   end
 
-  it 'deve calcular a média de 2 números negativos e retornar nota inválida' do
-    expect(media(-0.1,8)).to eq("nota inválida")
+  it 'deve calcular a média de 2 números sendo n2 negativo e retornar nota inválida' do
+    expect(media(0,-0.1)).to eq("nota inválida")
   end
 
-  it 'deve calcular média de 2 números positivos e retornar reprovado' do
-    expect(media(2.5,4)).to eq("reprovado")
-  end
-
-  it 'deve calcular média de 2 números positivos maior que 10 e retornar nota inválida' do
+  it 'deve calcular média de 2 números positivos sendo n1 maior que 10 e retornar nota inválida' do
     expect(media(10.1,10)).to eq("nota inválida")
+  end
+
+  it 'deve calcular média de 2 números positivos sendo n2 maior que 10 e retornar nota inválida' do
+    expect(media(10,10.1)).to eq("nota inválida")
   end
 
   it 'deve calcular média de 2 números positivos e retornar reprovado' do 
     expect(media(5.9,6)).to eq("reprovado")
   end
 
-  it 'deve calcular média de 2 números positivos e retornar reprovado' do 
-    expect(media(0,0)).to eq("reprovado")
+  it 'deve calcular média de 2 números positivos e retornar reprovado' do
+    expect(media(2.5,4)).to eq("reprovado")
   end
 
-  it 'deve calcular média de 2 números positivos e retornar aprovado' do 
-    expect(media(10,10)).to eq("aprovado")
+  it 'deve calcular a média de 2 números positivos e retornar aprovado' do
+    expect(media(6, 6)).to eq("aprovado")
+  end
+
+  it 'deve calcular a média de 2 números positivos e retornar aprovado' do
+    expect(media(8, 10)).to eq("aprovado")
   end
 end
