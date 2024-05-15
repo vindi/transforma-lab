0b827843971178938419854f14c6375d56b44a2d
Lucas Oristanio Faes <167210306+Oristanio-Lucas@users.noreply.github.com>
2024-05-15 11:57:41 -0300

    Trocando aspas duplas por aspas simples nos resultados esperados do teste

--- aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
+++ aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
@@ -4,39 +4,39 @@ require 'aprovado_ou_reprovado'
 describe 'Aprovado ou reprovado' do
   context 'Deve calcular a média de 2 números e retornar nota inválida' do
     it 'Quando n1 possuir valor negativo' do
-      expect(media(-0.1,0)).to eq("nota inválida")
+      expect(media(-0.1,0)).to eq('nota inválida')
     end
 
     it 'Quando n2 possuir valor negativo' do
-      expect(media(0,-0.1)).to eq("nota inválida")
+      expect(media(0,-0.1)).to eq('nota inválida')
     end
 
     it 'Quando n1 possuir valor maior que 10' do
-      expect(media(10.1,10)).to eq("nota inválida")
+      expect(media(10.1,10)).to eq('nota inválida')
     end
 
     it 'Quando n2 possuir valor maior que 10' do
-      expect(media(10,10.1)).to eq("nota inválida")
+      expect(media(10,10.1)).to eq('nota inválida')
     end
   end
 
   context 'Deve calcular a média de 2 números positivos e retornar reprovado' do
     it 'Quando a média for o máximo para retornar reprovado' do 
-      expect(media(5.9,6)).to eq("reprovado")
+      expect(media(5.9,6)).to eq('reprovado')
     end
 
     it 'Quando n1 ou n2 for igual a 0' do 
-      expect(media(0,0)).to eq("reprovado")
+      expect(media(0,0)).to eq('reprovado')
     end
   end
 
   context 'Deve calcular a média de 2 números positivos e retornar aprovado'do
     it 'Quando a média for o minímo para retornar aprovado' do
-      expect(media(6, 6)).to eq("aprovado")
+      expect(media(6, 6)).to eq('aprovado')
     end
 
     it 'Quando n1 ou n2 for igual a 10' do 
-      expect(media(10,10)).to eq("aprovado")
+      expect(media(10,10)).to eq('aprovado')
     end
   end
 
