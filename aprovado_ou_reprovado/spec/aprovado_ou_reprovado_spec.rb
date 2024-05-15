90a6ba2d34352a4534daa425decb120c052907e4
Oristanio-Lucas <lucas.faes@vindi.com.br>
2024-05-15 09:35:37 -0300

    Adicionando contexto nos cenários de testes que retornam nota inválida

--- aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
+++ aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
@@ -2,21 +2,22 @@ require 'spec_base'
 require 'aprovado_ou_reprovado'
 
 describe 'Aprovado ou reprovado' do
+  context 'Deve calcular a média de 2 números e retornar nota inválida' do
+    it 'Quando n1 possuir valor negativo' do
+      expect(media(-0.1,0)).to eq("nota inválida")
+    end
 
-  it 'deve calcular a média de 2 números sendo n1 negativo e retornar nota inválida' do
-    expect(media(-0.1,0)).to eq("nota inválida")
-  end
+    it 'Quando n2 possuir valor negativo' do
+      expect(media(0,-0.1)).to eq("nota inválida")
+    end
 
-  it 'deve calcular a média de 2 números sendo n2 negativo e retornar nota inválida' do
-    expect(media(0,-0.1)).to eq("nota inválida")
-  end
+    it 'Quando n1 possuir valor maior que 10' do
+      expect(media(10.1,10)).to eq("nota inválida")
+    end
 
-  it 'deve calcular média de 2 números positivos sendo n1 maior que 10 e retornar nota inválida' do
-    expect(media(10.1,10)).to eq("nota inválida")
-  end
-
-  it 'deve calcular média de 2 números positivos sendo n2 maior que 10 e retornar nota inválida' do
-    expect(media(10,10.1)).to eq("nota inválida")
+    it 'Quando n2 possuir valor maior que 10' do
+      expect(media(10,10.1)).to eq("nota inválida")
+    end
   end
 
   it 'deve calcular média de 2 números positivos e retornar reprovado' do 
@@ -32,6 +33,6 @@ describe 'Aprovado ou reprovado' do
   end
 
   it 'deve calcular a média de 2 números positivos e retornar aprovado' do
-    expect(media(8, 10)).to eq("aprovado")
+    expect(media(8, 10)).to eq("aprovado") 
   end
 end
