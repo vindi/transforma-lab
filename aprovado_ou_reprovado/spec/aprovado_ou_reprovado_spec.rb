d06f6e790b9e11aeaf3f4f499a0542751085884f
Oristanio-Lucas <lucas.faes@vindi.com.br>
2024-05-20 13:58:21 -0300

    Alterando o início texto do it no teste para caixa baixa

--- aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
+++ aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
@@ -3,40 +3,38 @@ require 'aprovado_ou_reprovado'
 
 describe 'Aprovado ou reprovado' do
   context 'Deve calcular a média de 2 números e retornar nota inválida' do
-    it 'Quando n1 possuir valor negativo' do
+    it 'quando n1 possuir valor negativo' do
       expect(media(-0.1,0)).to eq('nota inválida')
     end
 
-    it 'Quando n2 possuir valor negativo' do
+    it 'quando n2 possuir valor negativo' do
       expect(media(0,-0.1)).to eq('nota inválida')
     end
 
-    it 'Quando n1 possuir valor maior que 10' do
-      expect(media(10.1,10)).to eq('nota inválida')
+    it 'quando n1 possuir valor maior que 10' do
+      expect(media(10.1,0)).to eq('nota inválida')
     end
 
-    it 'Quando n2 possuir valor maior que 10' do
-      expect(media(10,10.1)).to eq('nota inválida')
+    it 'quando n2 possuir valor maior que 10' do
+      expect(media(0,10.1)).to eq('nota inválida')
     end
   end
 
   context 'Deve calcular a média de 2 números positivos e retornar reprovado' do
-    it 'Quando a média for o máximo para retornar reprovado' do
-      expect(media(5.9,6)).to eq('reprovado')
+    it 'quando a média for menor que 6 e notas diferentes de 0' do
+      expect(media(5.99,6)).to eq('reprovado')
     end
-
-    it 'Quando n1 ou n2 for igual a 0' do
+    it 'quando ambas as notas forem 0' do
       expect(media(0,0)).to eq('reprovado')
     end
   end
 
   context 'Deve calcular a média de 2 números positivos e retornar aprovado'do
-    it 'Quando a média for o minímo para retornar aprovado' do
+    it 'quando a média for maior ou igual a 6 e diferentes de 10' do
       expect(media(6, 6)).to eq('aprovado')
     end
-
-    it 'Quando n1 ou n2 for igual a 10' do
-      expect(media(10,10)).to eq('aprovado')
+    it 'quando ambas as notas forem 10' do
+      expect(media(10, 10)).to eq('aprovado')
     end
   end
 
