2ed6d087166a7a9553bcbd36bdb7e5670d502775
Oristanio-Lucas <lucas.faes@vindi.com.br>
2024-05-21 13:34:40 -0300

    Deletando método para calcular média e definindo método apenas para condicional

--- aprovado_ou_reprovado/README.md
+++ aprovado_ou_reprovado/README.md
@@ -1,16 +1,16 @@
 # Aprovado ou reprovado
 
 ## Sobre o exercício
-Escrever um método "media" que deve receber duas notas, calcular a média e retornar os seguintes cenários:
+Escrever um método "imprime_resultado" que deve receber uma média e retornar os seguintes cenários:
 - "aprovado" caso a média seja maior ou igual a 6;
 - "reprovado" caso a média seja menor que 6;
-- As notas devem ser entre 0 e 10, caso contrário retorne "nota inválida".
+- "nota inválida" caso a média seja menor que 0, maior que 10 ou não seja um valor numérico.
 
 ## Para começar
 - Abra o terminal;
 - Navegue até a pasta desse exercício utilizando os comandos `ls` e `cd`;
 - Execute o comando `config/setup`;
 - Execute o comando `rspec` (nesse ponto, várias falhas devem aparecer na sua tela);
-- No arquivo `lib/aprovado_ou_reprovado.rb`, implemente o método `media`;
+- No arquivo `lib/aprovado_ou_reprovado.rb`, implemente o método `imprime_resultado`;
 - Reexecute o comando `rspec`;
 - Repita esse processo até que todos os casos de teste sejam satisfeitos.
--- aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
+++ aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
@@ -3,39 +3,36 @@ require 'aprovado_ou_reprovado'
 
 describe 'Aprovado ou reprovado' do
   context 'Deve calcular a média de 2 números e retornar nota inválida' do
-    it 'quando n1 possuir valor negativo' do
-      expect(media(-0.1,0)).to eq('nota inválida')
+    it 'quando a média possuir valor negativo' do
+      expect(imprime_resultado(-0.1)).to eq('nota inválida')
     end
 
-    it 'quando n2 possuir valor negativo' do
-      expect(media(0,-0.1)).to eq('nota inválida')
+    it 'quando a média possuir valor maior que 10' do
+      expect(imprime_resultado(10.1)).to eq('nota inválida')
     end
 
-    it 'quando n1 possuir valor maior que 10' do
-      expect(media(10.1,0)).to eq('nota inválida')
-    end
-
-    it 'quando n2 possuir valor maior que 10' do
-      expect(media(0,10.1)).to eq('nota inválida')
+    it 'quando a média possuir um valor diferente de número' do
+      expect(imprime_resultado("10")).to eq('nota inválida')
     end
   end
 
   context 'Deve calcular a média de 2 números positivos e retornar reprovado' do
-    it 'quando a média for menor que 6 e notas diferentes de 0' do
-      expect(media(5.99,6)).to eq('reprovado')
+    it 'quando a média for menor que 6' do
+      expect(imprime_resultado(5.9999)).to eq('reprovado')
     end
-    it 'quando ambas as notas forem 0' do
-      expect(media(0,0)).to eq('reprovado')
+
+    it 'quando a média for 0 (mínimo para reprovado)' do
+      expect(imprime_resultado(0)).to eq('reprovado')
     end
   end
 
-  context 'Deve calcular a média de 2 números positivos e retornar aprovado'do
-    it 'quando a média for maior ou igual a 6 e diferentes de 10' do
-      expect(media(6, 6)).to eq('aprovado')
+  context 'Deve calcular a média de 2 números positivos e retornar aprovado' do
+    it 'quando a média for maior ou igual a 6' do
+      expect(imprime_resultado(6)).to eq('aprovado')
     end
-    it 'quando ambas as notas forem 10' do
-      expect(media(10, 10)).to eq('aprovado')
+
+    it 'quando a média for 10 (máximo para aprovado)' do
+      expect(imprime_resultado(10)).to eq('aprovado')
     end
   end
-
 end
