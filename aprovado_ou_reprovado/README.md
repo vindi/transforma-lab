8715b14a2b29c0b1988c529729075c7b2dc30528
Lucas Oristanio Faes <167210306+Oristanio-Lucas@users.noreply.github.com>
2024-05-31 10:24:20 -0300

    Adiciona exercicio aprovado ou reprovado (#1)
    * Adiciona exercicio aprovado ou reprovado
    
    * Corrigindo validações de testes
    
    * Implementando todos os cenários de erro 'nota inválida' no teste
    
    * Update aprovado_ou_reprovado/README.md
    
    Co-authored-by: KarinaSakata <127796651+KarinaSakata@users.noreply.github.com>
    
    * Adicionando contexto nos cenários de testes que retornam nota inválida
    
    * Eliminando redundâncias no código de testes
    
    * Adicionando validação de condições de nota mínima e máxima nos testes
    
    * Update README.md - Listando os possíveis cenários de teste no readme sobre o exercício
    
    * Trocando aspas duplas por aspas simples nos resultados esperados do teste
    
    * Removendo espaços em branco.
    
    * Alterando o início texto do it no teste para caixa baixa
    
    * Deletando método para calcular média e definindo método apenas para condicional
    
    * Ajustando textos dos contextos no teste
    
    ---------
    
    Co-authored-by: KarinaSakata <127796651+KarinaSakata@users.noreply.github.com>

---
+++ aprovado_ou_reprovado/README.md
@@ -0,0 +1,16 @@
+# Aprovado ou reprovado
+
+## Sobre o exercício
+Escrever um método "imprime_resultado" que deve receber uma média e retornar os seguintes cenários:
+- "aprovado" caso a média seja maior ou igual a 6;
+- "reprovado" caso a média seja menor que 6;
+- "nota inválida" caso a média seja menor que 0, maior que 10 ou não seja um valor numérico.
+
+## Para começar
+- Abra o terminal;
+- Navegue até a pasta desse exercício utilizando os comandos `ls` e `cd`;
+- Execute o comando `config/setup`;
+- Execute o comando `rspec` (nesse ponto, várias falhas devem aparecer na sua tela);
+- No arquivo `lib/aprovado_ou_reprovado.rb`, implemente o método `imprime_resultado`;
+- Reexecute o comando `rspec`;
+- Repita esse processo até que todos os casos de teste sejam satisfeitos.
---
+++ aprovado_ou_reprovado/spec/aprovado_ou_reprovado_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_base'
+require 'aprovado_ou_reprovado'
+
+describe 'Aprovado ou reprovado' do
+  context 'Deve receber dados inválidos e retornar nota inválida' do
+    it 'quando a média possuir valor negativo' do
+      expect(imprime_resultado(-0.1)).to eq('nota inválida')
+    end
+
+    it 'quando a média possuir valor maior que 10' do
+      expect(imprime_resultado(10.1)).to eq('nota inválida')
+    end
+
+    it 'quando a média possuir um valor diferente de número' do
+      expect(imprime_resultado("10")).to eq('nota inválida')
+    end
+  end
+
+  context 'Deve receber uma média positiva e retornar reprovado' do
+    it 'quando a média for menor que 6' do
+      expect(imprime_resultado(5.9999)).to eq('reprovado')
+    end
+
+    it 'quando a média for 0 (mínimo para reprovado)' do
+      expect(imprime_resultado(0)).to eq('reprovado')
+    end
+  end
+
+  context 'Deve receber uma média positiva e retornar aprovado' do
+    it 'quando a média for maior ou igual a 6' do
+      expect(imprime_resultado(6)).to eq('aprovado')
+    end
+
+    it 'quando a média for 10 (máximo para aprovado)' do
+      expect(imprime_resultado(10)).to eq('aprovado')
+    end
+  end
+end
